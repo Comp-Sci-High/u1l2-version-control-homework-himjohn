a944fb43e7d51232b292decc6fdae36d2672661d
himjohn <john.polonia25@compscihigh.org>
2024-09-27 18:37:59 +0000

    Final Completion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Version Control Assessment
 // 1. Explain what version control is.
 // Write your answer as a string and store it in the variable below:
-let versionControlExplanation = "";
+let versionControlExplanation = "track your changes and checkpoints ";
 
 console.log("What is version control?");
 console.log(versionControlExplanation); 
@@ -15,14 +15,14 @@ console.log(versionControlExplanation);
 
 // 2. Explain what a Git repository is.
 // Write your answer as a string and store it in the variable below:
-let gitRepoExplanation = "";
+let gitRepoExplanation = " repository stores your code";
 
 console.log("What is a Git repository?");
 console.log(gitRepoExplanation); 
 
 // 3. Name the benefits of using version control.
 // Write your answer as an array of strings below, with each string being a benefit.
-let versionControlBenefits = [];
+let versionControlBenefits = [you can go back to your code];
 
 console.log("What are the benefits of using version control?");
 console.log(versionControlBenefits); 
@@ -34,7 +34,7 @@ console.log(versionControlBenefits);
 
 // 4. Explain the steps to pushing changes in Git.
 // Write your answer as an array of steps below, with each step being a string.
-let stepsToPushChanges = [];
+let stepsToPushChanges = [first you gotta use git add then use git commit to name the update you gon give to then , use push to put everything on the repo.];
 
 console.log("What are the steps to pushing changes in Git?");
 console.log(stepsToPushChanges); 
